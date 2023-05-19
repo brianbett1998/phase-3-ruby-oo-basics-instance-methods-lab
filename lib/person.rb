45efe30955c47af4f586f93b320b5e5326d22ea1
BRIAN BETT <+>
2023-05-19 11:59:46 +0300

    Completed

---
+++ lib/person.rb
@@ -0,0 +1,12 @@
+class Person
+    #Class person
+
+    #instance method definition
+
+    def talk
+        puts "Hello World!"
+    end
+    def walk
+        puts "The Person is walking"
+    end
+end
